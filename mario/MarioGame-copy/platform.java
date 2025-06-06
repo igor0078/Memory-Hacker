4395ec83b54cf92cad64dcfefecd36561b886ad8
Kaciak2006 <kacpersaniak48@gmail.com>
2025-06-06 10:14:43 +0200

    w

---
+++ mario/MarioGame-copy/platform.java
@@ -0,0 +1,30 @@
+/**
+ * Write a description of class platform here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class platform  
+{
+    // instance variables - replace the example below with your own
+    private int x;
+
+    /**
+     * Constructor for objects of class platform
+     */
+    public platform()
+    {
+    }
+
+    /**
+     * An example of a method - replace this comment with your own
+     * 
+     * @param  y   a sample parameter for a method
+     * @return     the sum of x and y 
+     */
+    public int sampleMethod(int y)
+    {
+        // put your code here
+        return x + y;
+    }
+}
